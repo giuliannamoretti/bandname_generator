bb4e50b02839e908db8f414feeec243410f71221
Giulianna Moretti <giulianna.omoretti@gmail.com>
2023-01-09 20:53:21 -0400

    mensagem

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator.')
+city = input("Which city did you grow up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_name)
